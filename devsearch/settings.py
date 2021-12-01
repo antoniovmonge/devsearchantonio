8ed64344a4135360bbb0590e604194869acba391
Antonio Vilchez <antoniovmonge@gmail.com>
2021-12-01 15:27:46 +0100

    allowed-hosts

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'devsearch-antoniovm.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'devsearch-antonio.herokuapp.com/']
 
 
 # Application definition
